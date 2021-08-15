6d069a0bb0d404fb4351878637ab04faaa92635e
kperv <kvpervova@gmail.com>
2021-08-15 16:13:50 +0700

    Add app logic

--- app/app.py
+++ app/app.py
@@ -5,18 +5,16 @@ from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/process', methods=['POST'])
+def process():
     if request.method == 'POST':
-        form = request.form
-        text = form['text']
-        number = form['number']
-        result = [text, number, text]
-        return render_template("index.html", result=result)
-    render_template("index.html")
+        text = request.form['text']
+        number = int(request.form['number'])
+        result = text[:number]
+    return render_template("index.html", result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
